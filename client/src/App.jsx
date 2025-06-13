599b9866fca55d2f027e13dcd5677d3d6beec598
hani <aragsanahmed06@gmail.com>
2025-06-13 21:08:24 +0300

    secound project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function App() {
   return (
     <div>
-      <h1 className='bg-red-500'>Welcome</h1>
+      <h1 className='bg-red-600'>Hello World</h1>
 
     </div>
   )
