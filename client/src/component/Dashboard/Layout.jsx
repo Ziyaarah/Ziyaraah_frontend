bae2042dd7af596cfddc2a41bd6676318a617551
hani <aragsanahmed06@gmail.com>
2025-07-14 20:40:51 +0300

    update pilgrimage action

--- client/src/component/Dashboard/Layout.jsx
+++ client/src/component/Dashboard/Layout.jsx
@@ -3,7 +3,6 @@ import { Outlet } from 'react-router-dom';
 import Sidebar from '../Sidebar/Sidebar';
 import Dashboard from '../../pages/Dashboard';
 import TripPlanner from '../../pages/TripPlanner';
-import ResourceLibrary from '../../pages/ResourceLibrary';
 
 
 
